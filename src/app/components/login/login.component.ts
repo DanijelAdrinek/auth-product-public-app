00e03b90d0267a975cba5e839d9f88d14a92e4da
DanijelAdrinek <dadrinek00@gmail.com>
2024-05-23 20:07:12 -0700

    made changes to login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  passwordValidationRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{2,})(?=.*\d{3,}).{8,}$/;
+
   loginForm = this.fb.group({
-    email: '',
-    password: ''
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(this.passwordValidationRegex)]]
   });
 
   constructor(private AuthService: AuthService, private fb: FormBuilder) {}
 
-  login() {
-    this.AuthService.login(this.loginForm.value.email!, this.loginForm.value.password!);
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.validateAllFormFields(this.loginForm);
+    } else {
+      this.AuthService.login(this.loginForm.value.email!, this.loginForm.value.password!);
+    }
+  }
+
+  validateAllFormFields(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      } else {
+        control?.markAsTouched({ onlySelf: true });
+      }
+    });
   }
 }
